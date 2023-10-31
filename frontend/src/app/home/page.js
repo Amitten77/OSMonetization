f431efe408890967d6e658eb156e3aae5dec2397
stevenqie <steven.qie@gmail.com>
2023-10-30 21:38:34 -0500

    github backend finished

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -76,9 +76,9 @@ export default function Home() {
         </div>
         <form action="#" className="mt-6 flex" onSubmit={handleSubmit}>
           <input
-            type="email"
-            name="email"
-            id="email"
+            type="text"
+            name="url"
+            id="url"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             placeholder=" Enter your Github Repository URL"
             value={inputURL}
--- github_backend/app.py
+++ github_backend/app.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from flask_cors import CORS #comment this on deployment
 from api.StatsHandler import StatsHandler
 
+
 app = Flask(__name__)
 CORS(app) #comment this on deployment
 api = Api(app)
@@ -13,3 +14,6 @@ def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(StatsHandler, '/flask/hello')
+
+if __name__ == '__main__':
+    app.run(port = 8000, debug=True)
